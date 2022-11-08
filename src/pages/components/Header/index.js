392245fbba8a341da0d9234adf9c193fa2387170
Niklas <niklasheimburger@gmail.com>
2022-11-08 21:35:15 +0100

    add SocialIcons to Header

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -4,10 +4,10 @@ import { SocialIcon } from "react-social-icons";
 export default function Header() {
   return (
     <StyledHeader>
-      <SocialIcon network="linkedin" />
-      <SocialIcon network="github" />
-      <SocialIcon network="email" />
-      <SocialIcon network="whatsapp" />
+      <SocialIcon bgColor="transparent" fgColor="white" network="linkedin" />
+      <SocialIcon bgColor="transparent" fgColor="white" network="github" />
+      <SocialIcon bgColor="transparent" fgColor="white" network="email" />
+      <SocialIcon bgColor="transparent" fgColor="white" network="whatsapp" />
     </StyledHeader>
   );
 }
