59bb000a263ee815e78a9ebb539b0448addb1c62
Niklas <niklasheimburger@gmail.com>
2022-11-08 22:08:35 +0100

    update Header styling

--- src/pages/components/Header/styled.js
+++ src/pages/components/Header/styled.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 const StyledHeader = styled.header`
   position: sticky;
   top: 0;
+  padding-top: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export default StyledHeader;
