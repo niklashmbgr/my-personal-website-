db485f6fcfe748d41bb5f031b828f5e0bb7dcd69
Niklas <niklasheimburger@gmail.com>
2022-11-09 15:03:14 +0100

    minor styling changes

--- src/pages/components/Header/styled.js
+++ src/pages/components/Header/styled.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   position: sticky;
-  top: 0;
-  padding-top: 0.5rem;
+  top: 0.5rem;
+  margin: 1rem;
+  border-radius: 20px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+  background-color: black;
 `;
 
 export default StyledHeader;
--- src/pages/components/TypeText/styled.js
+++ src/pages/components/TypeText/styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const StyledHeadline = styled.h1`
+  display: flex;
+  justify-content: center;
   font-size: 1.25rem;
   color: #2c3333;
 `;
