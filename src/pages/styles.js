c89e932f5d96487711856653f2632a8d64c8ebe0
Niklas <niklasheimburger@gmail.com>
2022-11-09 15:02:28 +0100

    change background color + implement new font-family

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  src: url('/fonts/poppins-v20-latin-500.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('/fonts/poppins-v20-latin-500.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('/fonts/poppins-v20-latin-500.woff2') format('woff2'), /* Super Modern Browsers */
+       url('/fonts/poppins-v20-latin-500.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/poppins-v20-latin-500.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/poppins-v20-latin-500.svg#Poppins') format('svg'); /* Legacy iOS */
+}
 	*,
 	*::before,
 	*::after {
@@ -10,10 +22,12 @@ export const GlobalStyle = createGlobalStyle`
 		font-size: 16px;
 	}
 	body {
+        font-family: "Poppins",sans-serif;
 		margin: 0;
         height: 100vh;
         font-size: 1rem;
-        background: black;
+        background: #e7f6f2;
     }
+
 
 `;
