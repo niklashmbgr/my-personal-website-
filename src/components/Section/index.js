de51a4523d390afe43eedde4c921eb65d9cd9484
Niklas <niklasheimburger@gmail.com>
2022-11-12 21:45:37 +0100

    implement ProjectCard

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -2,14 +2,31 @@ import StyledSection from "./styled";
 import ProfileImage from "../Image";
 import AnimatedDiv from "../AnimatedDiv/styled";
 import Article from "../Article";
+import ProjectCard from "../ProjectCard";
 
 export default function Section() {
   return (
     <StyledSection>
       <ProfileImage />
-      <AnimatedDiv variant="about-me">
+      <AnimatedDiv
+        variant="about-me"
+        initial={{
+          y: 500,
+          opacity: 0,
+          scale: 1,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
         <Article />
       </AnimatedDiv>
+      <ProjectCard />
     </StyledSection>
   );
 }
