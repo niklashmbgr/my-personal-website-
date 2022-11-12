4ba35f11436c8d4a12021e4af6398bf3642786a9
Niklas <niklasheimburger@gmail.com>
2022-11-12 20:23:25 +0100

    add about variant

--- src/components/AnimatedDiv/styled.js
+++ src/components/AnimatedDiv/styled.js
@@ -1,6 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
-const AnimatedDiv = styled(motion.div)``;
+const AnimatedDiv = styled(motion.div)`
+  ${({ variant }) =>
+    variant === "about-me" &&
+    css`
+      padding: 0.5rem;
+      margin-top: 1rem;
+      border-radius: 2rem;
+      background-color: #99c4c8;
+    `}
+`;
 
 export default AnimatedDiv;
