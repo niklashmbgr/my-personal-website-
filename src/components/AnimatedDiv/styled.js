8d9c4aceec663cd5f1a6cbee6a28a83fa0302f63
Niklas <niklasheimburger@gmail.com>
2022-11-12 23:20:24 +0100

    add contact variant

--- src/components/AnimatedDiv/styled.js
+++ src/components/AnimatedDiv/styled.js
@@ -18,6 +18,14 @@ const AnimatedDiv = styled(motion.div)`
       border-radius: 2rem;
       background-color: #c7d36f;
     `}
+    ${({ variant }) =>
+    variant === "contact-me" &&
+    css`
+      padding: 0.5rem;
+      margin-top: 1rem;
+      border-radius: 2rem;
+      background-color: #ff7171;
+    `}
 `;
 
 export default AnimatedDiv;
