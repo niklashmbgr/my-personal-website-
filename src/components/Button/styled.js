d508c0b273708a7ee0b82e3cfa3e4946a9b85fa4
Niklas <niklasheimburger@gmail.com>
2022-11-12 23:33:39 +0100

    add send variant

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,10 +1,19 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledButton = styled.button`
   background: transparent;
   border: none;
   font-family: "Poppins";
   font-size: 1.5rem;
+
+  ${({ variant }) =>
+    variant === "send" &&
+    css`
+      padding: 0.5rem;
+      margin-top: 1rem;
+      border-radius: 2rem;
+      background-color: #ff7171;
+    `}
 `;
 
 export default StyledButton;
