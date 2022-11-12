41d9fa06c59e4bfbe0a85c977e12aabb72c66413
Niklas <niklasheimburger@gmail.com>
2022-11-12 22:13:07 +0100

    add font-family + size to button

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StyledButton = styled.button`
   background: transparent;
   border: none;
+  font-family: "Poppins";
+  font-size: 1.5rem;
 `;
 
 export default StyledButton;
