6302f942774671660edb6c0fa16cc4708f642ad1
Niklas <niklasheimburger@gmail.com>
2022-11-15 00:53:06 +0100

    change Typetext + implement into Section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -2,6 +2,7 @@ import StyledSection from "./styled";
 import Article from "../Article";
 import ProjectCard from "../ProjectCard";
 import ContactForm from "../ContactForm";
+import TypeText from "../TypeText/index";
 
 export default function Section() {
   return (
