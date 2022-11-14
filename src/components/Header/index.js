a53c6aebb63b66ad9212c8a7d10800dc173cddc7
Niklas <niklasheimburger@gmail.com>
2022-11-15 00:49:02 +0100

    redesign Header + add StyledSpan

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,54 @@
 import StyledHeader from "./styled";
 import { SocialIcon } from "react-social-icons";
 import AnimatedDiv from "../AnimatedDiv/styled";
+import StyledSpan from "../StyledSpan/styled";
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <AnimatedDiv>
+    <StyledHeader
+      initial={{
+        opacity: 0,
+        scale: 0,
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        duration: 0.6,
+      }}
+    >
+      <AnimatedDiv
+        initial={{
+          opacity: 1,
+          scale: 0,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
+        <StyledSpan>menu</StyledSpan>
+      </AnimatedDiv>
+      <AnimatedDiv
+        initial={{
+          opacity: 1,
+          scale: 0,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
         <SocialIcon bgColor="transparent" fgColor="white" network="linkedin" />
         <SocialIcon bgColor="transparent" fgColor="white" network="github" />
-      </AnimatedDiv>
-      <AnimatedDiv>
         <SocialIcon bgColor="transparent" fgColor="white" network="email" />
-        <SocialIcon bgColor="transparent" fgColor="white" network="whatsapp" />
       </AnimatedDiv>
     </StyledHeader>
   );
