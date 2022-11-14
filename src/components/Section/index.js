227a8d2cd72881bfefc93ed4eb06dd9e4c9d4e4e
Niklas <niklasheimburger@gmail.com>
2022-11-15 00:55:04 +0100

    delete TypeText from Section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -2,7 +2,6 @@ import StyledSection from "./styled";
 import Article from "../Article";
 import ProjectCard from "../ProjectCard";
 import ContactForm from "../ContactForm";
-import TypeText from "../TypeText/index";
 
 export default function Section() {
   return (
