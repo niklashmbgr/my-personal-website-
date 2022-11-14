832bcfc2fe87740564707fdb8fe92e8cc4487656
Niklas <niklasheimburger@gmail.com>
2022-11-15 00:57:19 +0100

    add profile image

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -2,10 +2,19 @@ import StyledSection from "./styled";
 import Article from "../Article";
 import ProjectCard from "../ProjectCard";
 import ContactForm from "../ContactForm";
+import memoji from "../../../public/memoji.png";
+import Image from "next/image";
 
 export default function Section() {
   return (
     <StyledSection>
+      <Image
+        src={memoji}
+        alt="profile memoji"
+        width={240}
+        height={220}
+        priority
+      />
       <Article />
       <ProjectCard />
       <ContactForm />
